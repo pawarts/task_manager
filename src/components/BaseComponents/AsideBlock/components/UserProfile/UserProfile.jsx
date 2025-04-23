81b291ce35aafe0a932f99a5999af136ca50f160
pawarts <98350667+pawarts@users.noreply.github.com>
2025-04-23 15:08:10 +0300

    some fix

--- src/components/BaseComponents/AsideBlock/components/UserProfile/UserProfile.jsx
+++ src/components/BaseComponents/AsideBlock/components/UserProfile/UserProfile.jsx
@@ -5,11 +5,11 @@ import s from "./styles/UserProfile.module.css"
 const UserProfile = (props) => {
      return (
           <div className={s.wrapper}>
-               <UserIcon avatar="https://i.pinimg.com/474x/60/5b/9b/605b9b86a82dd0147ed8aa612381326f.jpg" />
+               <UserIcon avatar="https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://media.easy-peasy.ai/d9a4f9cd-738b-4b49-b5a3-b887e68162d2/bb4a89de-a3a4-491a-9f78-086388c85002.png" />
 
                <div className={s.user_info}>
-                    <h3>Andrii Rykachevskyi</h3>
-                    <p>Developer</p>
+                    <h3>[Name] [Surname]</h3>
+                    <p>position</p>
                </div>
           </div>
      );
